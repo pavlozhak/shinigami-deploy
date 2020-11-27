8e507c11f4b82facdea63a72353a3024936fc90a
pavlozhak <pavlozhak@gmail.com>
2020-11-28 00:40:06 +0200

    add dashboard controller, base views, change route

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,17 @@
+@extends('base')
+
+@section('page-title', 'Dashboard')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3>
+                    Shinigami Deploy
+                </h3>
+            </div>
+        </div>
+    </div>
+
+@endsection
